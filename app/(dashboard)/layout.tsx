77aaeb57f765493e2b88b7b83e8490b8b39d8735
oluoko <brianotieno586@gmail.com>
2024-10-11 17:49:42 +0300

    Mood Journal: Creating the Journal Page

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,15 @@
 const DashboardLayout = ({ children }) => {
   return (
-    <div className="h-screen w-screen relative">
-      <aside className="absolute top-0 left-0 h-full w-screen border-r border-black/10">
-        {' '}
+    <div className="h-screen w-screen relative bg-slate-950 text-slate-300 ">
+      <aside className="absolute top-0 left-0 h-full w-[200px] border-r border-slate-400/30">
         Mood
       </aside>
-      {children}
+      <div className="ml-[200px]">
+        <header className="p-4 h-[60px] border-b border-slate-400/30">
+          <h1>Journal</h1>
+        </header>
+        <div className="">{children}</div>
+      </div>
     </div>
   )
 }
