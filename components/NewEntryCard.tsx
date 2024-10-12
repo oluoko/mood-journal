bdb6b6998e2b574b3174b73607fa050b2f6734af
oluoko <brianotieno586@gmail.com>
2024-10-12 06:29:06 +0300

    Mood Journal: Creating a New Entry

--- components/NewEntryCard.tsx
+++ components/NewEntryCard.tsx
@@ -1,6 +1,20 @@
+'use client'
+
+import { createNewEntry } from '@/utils/api'
+import { useRouter } from 'next/navigation'
+
 const NewEntryCard = () => {
+  const router = useRouter()
+
+  const handleOnClick = async () => {
+    const data = await createNewEntry()
+    router.push(`/journal/${data.id}`)
+  }
   return (
-    <div className="cursor-pointer overflow-hidden rounded-lg border border-slate-400/30">
+    <div
+      className="cursor-pointer overflow-hidden rounded-lg border border-slate-400/30"
+      onClick={handleOnClick}
+    >
       <div className="p-3 md:p-6">
         <span className="text-lg md:text-2xl">New Entry</span>
       </div>
