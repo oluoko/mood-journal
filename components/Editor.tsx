1de8d058ce09c3e8152ee03abbfb50d5c860b0a4
oluoko <brianotieno586@gmail.com>
2024-10-12 07:51:47 +0300

    Mood Journal: Journal Entry Editor

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -6,7 +6,11 @@ const Editor = ({ entry }) => {
   const [value, setValue] = useState(entry.content)
   return (
     <div className="w-full h-full">
-      <textarea value={value} />
+      <textarea
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="bg-slate-700/40 w-full max-h-full p-3 md:p-7 text-lg md:text-xl rounded-lg outline-none"
+      />
     </div>
   )
 }
