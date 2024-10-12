9f32c0fbfa97c174dcad5662d2a9e754b77f9ad5
oluoko <brianotieno586@gmail.com>
2024-10-12 06:57:03 +0300

    Mood Journal: EntryCard Component UI

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -2,6 +2,7 @@ import EntryCard from '@/components/EntryCard'
 import NewEntryCard from '@/components/NewEntryCard'
 import { getUserByClerkId } from '@/utils/auth'
 import { prisma } from '@/utils/db'
+import Link from 'next/link'
 
 const getEntries = async () => {
   const user = await getUserByClerkId()
@@ -24,10 +25,12 @@ const JournalPage = async () => {
   return (
     <div className=" md:p-8">
       <h2 className="text-2xl md:text-3xl mb-6 md:mb-10">Journal</h2>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-2 md:gap-4">
         <NewEntryCard />
         {entries.map((entry) => (
-          <EntryCard key={entry.id} entry={entry} />
+          <Link href={`/journal/${entry.id}`} key={entry.id}>
+            <EntryCard key={entry.id} entry={entry} />
+          </Link>
         ))}
       </div>
     </div>
--- components/EntryCard.tsx
+++ components/EntryCard.tsx
@@ -1,9 +1,13 @@
 const EntryCard = ({ entry }) => {
+  const date = new Date(entry.createdAt).toDateString()
   return (
-    <div>
-      <h1>{entry.title}</h1>
-      <p>Entry Id: {entry.id}</p>
-      <p>{entry.content}</p>
+    <div className="divide-y divide-slate-400/30 overflow-hidden rounded-lg border border-slate-400/30 bg-slate-700/40 ">
+      <div className="px-4 py-5 sm:px-6">{date}</div>
+      <div className="px-4 py-5 sm:px-6">summary</div>
+      <div className="px-4 py-5 sm:px-6">mood</div>
+      <div className="px-4 py-4 sm:px-6 max-h-[70px] overflow-hidden">
+        {entry.content}
+      </div>
     </div>
   )
 }
