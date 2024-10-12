bb4cee87fa7c99868382c6f047bfb124c60a404a
oluoko <brianotieno586@gmail.com>
2024-10-12 16:59:07 +0300

    Mood Journal: langchain installation

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,7 +1,7 @@
-import { OpenAI } from 'langchain/llms/openai'
+import { OpenAI } from '@langchain/openai'
 
 export const analyze = async (prompt) => {
   const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' })
-  const result = await model.complete(prompt)
+  const result = await model.call(prompt)
   console.log(result)
 }
