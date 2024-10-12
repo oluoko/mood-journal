ca9acfe013d1234963c04068469900211c0a27c7
oluoko <brianotieno586@gmail.com>
2024-10-12 07:43:45 +0300

    Mood Journal: Displaying Entry Details

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,5 +1,28 @@
-const EntryPage = ({ params }) => {
-  return <div>{params.id}</div>
+import Editor from '@/components/Editor'
+import { getUserByClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+
+const getEntry = async (id) => {
+  const user = await getUserByClerkId()
+  const entry = prisma.journalEntry.findUnique({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id,
+      },
+    },
+  })
+
+  return entry
+}
+
+const EntryPage = async ({ params }) => {
+  const entry = await getEntry(params.id)
+  return (
+    <div>
+      <Editor entry={entry} />
+    </div>
+  )
 }
 
 export default EntryPage
