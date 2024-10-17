c25aa3879798426845b46932eebeabeddcdece07
oluoko <brianotieno586@gmail.com>
2024-10-17 14:25:10 +0300

    Mood Journal: Structured Output with Zod :

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,4 +1,25 @@
 import { OpenAI } from '@langchain/openai'
+import { StructuredOutputParser } from '@langchain/core/output_parsers'
+import z from 'zod'
+
+const parser = StructuredOutputParser.fromZodSchema(
+  z.object({
+    mood: z
+      .string()
+      .describe('the mood of the perseon who wrote the journal entry.'),
+    summary: z.string().describe('quick summary of the entire entry.'),
+    negative: z
+      .boolean()
+      .describe(
+        'is the journal entry negative? (i.e. does it contain negative emotions?).'
+      ),
+    color: z
+      .string()
+      .describe(
+        'a hexidecimal color code that represents the mood of the entry. Example $0101fe for blue for representing happiness.'
+      ),
+  })
+)
 
 export const analyze = async (prompt) => {
   const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' })
