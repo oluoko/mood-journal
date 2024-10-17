113fddcaf9922930ee61d25f41feb4431201c2b0
oluoko <brianotieno586@gmail.com>
2024-10-17 15:49:33 +0300

    Mood Journal: Home page logo update

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,29 @@
 import { auth } from '@clerk/nextjs/server'
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default async function Home() {
   const { userId } = await auth()
   const href = userId ? '/journal' : '/new-user'
 
   return (
-    <div className="w-screen h-screen bg-slate-950 flex justify-center items-center text-slate-300">
-      <div className="w-full max-w-[320px] md:max-w-[89%] mx-auto">
-        <h1 className="text-4xl md:text-6xl mb-[30px] md:mb-[70px]">
+    <div className="w-screen h-screen bg-slate-950  text-slate-300 flex flex-col justify-center items-center gap-10 md:gap-15">
+      <div className="w-full max-w-[90%] md:max-w-[70%] mx-auto ">
+        <Image
+          src="/Reflectify W logo.gif"
+          alt="Reflectify Logo"
+          width={320}
+          height={180}
+          className="w-80 md:w-[500px] "
+        />
+        <div className="text-4xl md:text-5xl font-bold mb-4">
           The Best Journal app out there
-        </h1>
+        </div>
+      </div>
+      <div
+        className="w-full max-w-[90%] md:max-w-[70%] mx-auto
+    "
+      >
         <p className="text-xl md:text-2xl text-slate-300/50 mb-4">
           This is best place to keep track of you mood though out your life. All
           you have to do is be honest
