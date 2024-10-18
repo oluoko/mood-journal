7445ab36f15ad0e06b3dad412fcc9ce646f461a8
oluoko <brianotieno586@gmail.com>
2024-10-18 18:50:08 +0300

    Mood Journal:  Similarity Searches with Vectors

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,7 +1,10 @@
-import { OpenAI } from '@langchain/openai'
+import { OpenAI, OpenAIEmbeddings } from '@langchain/openai'
 import { StructuredOutputParser } from '@langchain/core/output_parsers'
 import z from 'zod'
 import { PromptTemplate } from '@langchain/core/prompts'
+import { Document } from 'langchain/document'
+import { MemoryVectorStore } from 'langchain/vectorstores/memory'
+import { loadQARefineChain } from 'langchain/chains'
 
 const parser = StructuredOutputParser.fromZodSchema(
   z.object({
@@ -50,4 +53,25 @@ export const analyze = async (content) => {
   } catch (error) {
     console.log(error)
   }
+}
+
+const qa = async (question, entries) => {
+  const docs = entries.map(
+    (entry) =>
+      new Document({
+        pageContent: entry.content,
+        metadata: { id: entry.id, date: entry.createdAt },
+      })
+  )
+  const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' })
+  const chain = loadQARefineChain(model)
+  const embeddings = new OpenAIEmbeddings()
+  const store = await MemoryVectorStore.fromDocuments(docs, embeddings)
+  const relevantDocs = await store.similaritySearch(question)
+  const res = await chain.call({
+    input_documents: relevantDocs,
+    question,
+  })
+
+  return res.output_text
 }
