7f0695b42b51036609c596d509f8da540299be69
oluoko <brianotieno586@gmail.com>
2024-10-19 10:49:56 +0300

    Mood Journal: Mocking Fuctions & Testing Homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,30 @@
-import { auth } from '@clerk/nextjs/server'
+'use client'
+import { useAuth } from '@clerk/nextjs'
 import Link from 'next/link'
 import Image from 'next/image'
 
-export default async function Home() {
-  const { userId } = await auth()
+export default function Home() {
+  const { userId } = useAuth()
   const href = userId ? '/journal' : '/new-user'
 
   return (
-    <div className="w-screen h-screen bg-slate-950  text-slate-300 flex flex-col justify-center items-center gap-10 md:gap-15">
-      <div className="w-full max-w-[90%] md:max-w-[70%] mx-auto ">
+    <div className="w-screen h-screen bg-slate-950 text-slate-300 flex flex-col justify-center items-center gap-10 md:gap-15">
+      <div className="w-full max-w-[90%] md:max-w-[70%] mx-auto">
         <Image
           src="/Reflectify W logo.gif"
           alt="Reflectify Logo"
           width={320}
           height={180}
-          className="w-80 md:w-[500px] "
+          className="w-80 md:w-[500px]"
         />
         <div className="text-4xl md:text-5xl font-bold mb-4">
           The Best Journal app out there
         </div>
       </div>
-      <div
-        className="w-full max-w-[90%] md:max-w-[70%] mx-auto
-    "
-      >
+      <div className="w-full max-w-[90%] md:max-w-[70%] mx-auto">
         <p className="text-xl md:text-2xl text-slate-300/50 mb-4">
-          This is best place to keep track of you mood though out your life. All
-          you have to do is be honest
+          This is the best place to keep track of your mood throughout your
+          life. All you have to do is be honest.
         </p>
         <div>
           <Link href={href}>
