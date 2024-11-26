0fca1c13f6117da92a2b2a4a21e78dfb91415034
oluoko <brianotieno586@gmail.com>
2024-11-26 21:06:10 +0300

    Mood Journal: Added and styled AuthImage.tsx component

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,17 @@
+import AuthImage from '@/components/AuthImage'
 import { SignUp } from '@clerk/nextjs'
 
 const SignUpPage = () => {
-  return <SignUp afterSignUpUrl="/new-user" redirectUrl="/new-user" />
+  return (
+    <div className="flex justify-between h-screen w-screen bg-slate-950 text-">
+      <div className="hidden md:flex w-3/5">
+        <AuthImage textColor="slate-300" />
+      </div>
+      <div className="flex flex-col justify-center items-center w-full md:w-2/5">
+        <SignUp afterSignUpUrl="/new-user" redirectUrl="/new-user" />
+      </div>
+    </div>
+  )
 }
 
 export default SignUpPage
